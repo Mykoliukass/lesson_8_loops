6781a1806db927a5fede4ad623c9daf0c0b93be2
Mykolas Jerutis <mykolasjerutis@email.com>
2023-10-11 20:08:09 +0300

    Add password-program.py updated

--- password-program.py
+++ password-program.py
@@ -7,17 +7,45 @@
 # If in password numbers or symbols are not provided, the program should add additional value to dictionary as a value, 
 # names 'improved password'
 # Count how many times the user inserted a wrong password
-# users_credentials = "username='mykolas2',password='jerutis2';username='eteris',password='uzimtas;username='burbulas',password='burbuliene;username='pilvas',password='didelis!;username='vardas',password='slaptazodis'"
+# username='mykolas2',password='jerutis2';username='eteris',password='uzimtas;username='burbulas',password='burbuliene1!;username='pilvas',password='didelis!;username='vardas',password='slaptazodis'
+
+import random
 
 users_credentials = input("Please insert a database to be used for users credentials: ")
 credential_pairs = users_credentials.split(";")
 cleaned_pairs = [pair.replace("'", "").strip() for pair in credential_pairs]
 credentials_dictionary = {}
-for single_pair in credential_pairs:
 
+for single_pair in cleaned_pairs:
+    username, password = single_pair.split(',')
+    username_value = username.split('=')[1].strip()
+    password_value = password.split('=')[1].strip()
+    credentials_dictionary[username_value] = password_value
 
-# while True:
-#     user_input = input("Enter your name: ")
-#     if len(user_input) != 0:
-#         break
-# print(f"You entered {user_input }")
+while True:
+    input_username = input("Please enter your username: ")
+    number_of_attempts = 0
+    if input_username in credentials_dictionary:
+        while True:
+            input_password = input(f"Hey {input_username}! Please enter your password: ")
+            if credentials_dictionary[input_username] == input_password:
+                special_symbols = "!@#$%^&*()-_=+[]{}|:'\".<>?/"
+                if any(symbol in special_symbols for symbol in input_password) and any(symbol.isdigit() for symbol in input_password):
+                    print("Login successful!")
+                else:
+                    if any(symbol in special_symbols for symbol in input_password):
+                        random_digit = str(random.randint(0, 9))
+                        improved_password = credentials_dictionary[input_username] + random_digit 
+                        print(f"Login successful! However, we would like to suggest an improved password: {improved_password}")
+                    else:
+                        random_symbol = random.choice(special_symbols)
+                        improved_password = credentials_dictionary[input_username] + random_symbol
+                        print(f"Login successful! However, we would like to suggest an improved password: {improved_password}")
+                break
+            else:
+                number_of_attempts += 1
+                print(f"Login failed. Incorrect password. You have tried to login {number_of_attempts} times")
+        break
+    else:
+        print("Login failed. Username not found.")
+
