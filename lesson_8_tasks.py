ed0dbe2537abf10c0a4fcb2f554cbad12b0dab09
Mykolas Jerutis <mykolasjerutis@email.com>
2023-10-12 20:50:40 +0300

    Update lesson_8_tasks.py

--- lesson_8_tasks.py
+++ lesson_8_tasks.py
@@ -22,16 +22,16 @@
 # Create a pin code cracker. Let's say pin code consists of 4 random digits. 
 # You can store the value in variable. Then create a loop going through all possible combinations until you find the one you entered.
 
-pin = '4981'
-locked = True
+# pin = '4981'
+# locked = True
 
-while locked:
-    for digit1 in range(10):
-        for digit2 in range(10):
-            for digit3 in range(10):
-                for digit4 in range(10):
-                    current_attempt = f"{digit1}{digit2}{digit3}{digit4}"
-                    # Check if the current combination matches the pin
-                    if current_attempt == pin:
-                        print(f"Pin Cracked! The correct pin is: {current_attempt}")
-                        locked = False
+# while locked:
+#     for digit1 in range(10):
+#         for digit2 in range(10):
+#             for digit3 in range(10):
+#                 for digit4 in range(10):
+#                     current_attempt = f"{digit1}{digit2}{digit3}{digit4}"
+#                     # Check if the current combination matches the pin
+#                     if current_attempt == pin:
+#                         print(f"Pin Cracked! The correct pin is: {current_attempt}")
+#                         locked = False
